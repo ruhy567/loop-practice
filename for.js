d6b7f8952e00776d3cdd325bfc421cbc8fc44d4b
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-18 21:08:17 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,11 @@
+const numbers = [12, 32,34,56,67,85];
+
+console.log (numbers);
+
+const icecream =['vanila', 'chocolate', 'strawberry', 'pistachio', 'mango', 'butterscosh' ];
+
+for(const flavour of icecream){
+    console.log(flavour);
+    console.log('yumm');
+    
+}
