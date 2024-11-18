41e1cc23af54c95575caed3138131a545b624d9f
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-19 01:24:17 +0600

    looping

---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 11;
+if (number < 10 ){
+    console.log("fqiowuhrf");
+
+}
+console.log("ruhhhhh");
+
+//-----while loop----
+let num = 0;
+while (num < 5){
+    console.log("fgehg",num);
+    num = num + 1;
+}
