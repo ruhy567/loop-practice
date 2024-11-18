eb3c0d8e588f8b6bce98a284bb11af34670000dd
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-19 02:05:24 +0600

    exercise

---
+++ for_loop.js
@@ -0,0 +1,13 @@
+/*
+ ---- for loop----
+for (1st part; 2nd part; 3rd part){
+        loop statement
+}
+*/
+for (let num = 0; num < 5; num++){
+        console.log(num);
+}
+
+for (let i = 0; i < 10; i++){
+    console.log(i);
+}
